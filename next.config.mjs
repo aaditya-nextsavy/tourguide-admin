29a69f0e5dc5f1edbeb020ed53ad1bb13f85a20a
Aaditya <aaditya@nextsavy.com>
2025-04-07 11:12:51 +0530

    added in next config domian names for the images

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-      domains: ["www.istockphoto.com"],
+      domains: ["www.istockphoto.com","www.pexels.com","images.pexels.com"],
     },
     async redirects() {
       return [
